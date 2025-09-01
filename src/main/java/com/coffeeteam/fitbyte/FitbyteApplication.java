8361fd99a898e43a6f8bec16bfc711ddaccd5d8f
Muhammad Ghazian <mghazian@github.com>
2025-09-01 07:32:43 +0700

    Initial commit

---
+++ src/main/java/com/coffeeteam/fitbyte/FitbyteApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeteam.fitbyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitbyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitbyteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeeteam/fitbyte/FitbyteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeteam.fitbyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitbyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
