1048337c79b735803db327f1ed792dc4e1daa607
Ridho Khalis <ridhokholis9a@gmail.com>
2025-09-03 23:29:47 +0700

    feat: implement controller advice

--- src/main/java/com/coffeeteam/fitbyte/file/FileService.java
+++ src/main/java/com/coffeeteam/fitbyte/file/FileService.java
@@ -2,8 +2,12 @@ package com.coffeeteam.fitbyte.file;
 
 import com.coffeeteam.fitbyte.storage.ObjectStorageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,15 +16,19 @@ public class FileService {
     private final ObjectStorageService storage;
 
     public FileResponse upload(MultipartFile file) {
-        if(file.isEmpty()){
-            throw new IllegalArgumentException("File is required");
+        if (file.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "File is required");
         }
-        if(file.getSize() > 100 * 1024){
-            throw new IllegalArgumentException("File size exceeds 100 KB");
+        if (file.getSize() > 100 * 1024) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "File size exceeds 100 KB");
         }
         String contentType = file.getContentType();
-        if (contentType == null || !(contentType.equals("image/jpeg") || contentType.equals("image/png"))) {
-            throw new IllegalArgumentException("Invalid file type, only jpeg/png allowed");
+        List<String> allowedTypes = List.of("image/jpeg", "image/jpg", "image/png");
+        if (contentType == null || !allowedTypes.contains(contentType)) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "Invalid file type, only jpeg/jpg/png allowed"
+            );
         }
 
         var result = storage.upload(file, "uploads");
